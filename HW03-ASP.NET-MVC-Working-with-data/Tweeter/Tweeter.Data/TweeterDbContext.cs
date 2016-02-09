03e9e812bf173d3d4ca7acd2ae72db052f7c102f
Marin Marinov <marin.telerik@gmail.com>
2016-02-09 11:25:04 +0200

    Project init, Users moved to Data layer, Generic repository pattern implemented

---
+++ HW03-ASP.NET-MVC-Working-with-data/Tweeter/Tweeter.Data/TweeterDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace Tweeter.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
+
+    public class TweeterDbContext : IdentityDbContext<User>
+    {
+        public TweeterDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static TweeterDbContext Create()
+        {
+            return new TweeterDbContext();
+        }
+    }
+}
