8613984909656548aca43373df68ff89504884f5
Marin Marinov <marin.telerik@gmail.com>
2016-02-11 13:54:06 +0200

    task 2 finally successfully deployed

---
+++ HW05-IIS-and-Azure-Deployment/Tweeter/README.md
@@ -0,0 +1,4 @@
+# Tweeter simple app
+
+###Please, test it on: [http://hwtweeter.azurewebsites.net/](http://hwtweeter.azurewebsites.net/)
+
---
+++ HW05-IIS-and-Azure-Deployment/Tweeter/Tweeter.Web/Models/CreateTweetModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Tweeter.Models
+{
+    public class CreateTweetModel
+    {
+        public string Message { get; set; }
+    }
+}
